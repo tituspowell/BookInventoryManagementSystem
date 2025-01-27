400d9c80f5f18abf713b1cfc3dbcb917a80f4162
Titus Powell <tituspowell@gmail.com>
2025-01-27 12:07:40 +0000

    Added the entity classes.

---
+++ BookInventoryManagementSystem.Data/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookInventoryManagementSystem.Data.Entities;
+
+public class Book : BaseEntity
+{
+    public required string Title { get; set; }
+    public required List<string> Authors { get; set; } = [];
+    public DateOnly? PublicationYear { get; set; }
+    public string? ISBN { get; set; }
+    public string? Genre { get; set; }
+    public string? CoverImageURL { get; set; }
+    public required List<string> Tags { get; set; } = [];
+}
---
+++ BookInventoryManagementSystem.Data/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace BookInventoryManagementSystem.Data.Entities;
+
+public class Review : BaseEntity
+{
+    public required int RatingOutOfFive { get; set; } // Rating could be a float to allow 4.5 for example but we force whole numbers for simplicity
+    public required string ReviewText { get; set; }
+
+    // Foreign Keys
+    public required Book Book { get; set; }
+    public required int BookId { get; set; }
+
+    public required ApplicationUser Reviewer { get; set; }
+    public required string ReviewerId { get; set; }
+}
